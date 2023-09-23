f666461576a510db63b0ba985feabd601ee924e2
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-09-23 13:35:50 +0200

    Added related items

--- item/views.py
+++ item/views.py
@@ -4,7 +4,9 @@ from .models import Category, Item
 
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
+    related_items = Item.objects.filter(category=item.category, is_reserved=False).exclude(pk=item.pk)[0:3]
 
     return render(request, 'item/detail.html', {
         'item': item,
+        'related_items': related_items,
     })
