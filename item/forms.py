189f267bf0450ec7c2490148f60a42ab7646491d
KajetanMieloch <91525250+KajetanMieloch@users.noreply.github.com>
2023-09-23 15:36:08 +0200

    Added new future, now loged user can add new hotel

---
+++ item/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+
+from .models import Category, Item
+
+INPUT_CLASSES = 'w-full px-3 py-2 border rounded shadow-inner'
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('name', 'description', 'price', 'category', 'image')
+        
+        widgets = {
+            'category': forms.Select(attrs={
+                'class': INPUT_CLASSES
+                }),
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+                }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+                }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+                }),
+            'price': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+                }),
+        }
--- item/views.py
+++ item/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 
+from .forms import NewItemForm
 from .models import Category, Item
 
 def detail(request, pk):
@@ -9,4 +11,22 @@ def detail(request, pk):
     return render(request, 'item/detail.html', {
         'item': item,
         'related_items': related_items,
+    })
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+            
+            return redirect('item:detail', pk=item.pk)
+    else:
+        form = NewItemForm()
+    
+    return render(request, 'item/new.html', {
+        'form': form,
+        'title': 'New Item',
     })
